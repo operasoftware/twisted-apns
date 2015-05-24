0d3113955eb32aeeb2e440a6e95fb71c549b9c99
Michał Łowicki <mlowicki@gmail.com>
2015-05-24 21:05:13 +0200

    Bump version to 0.4. Fix download URL

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ from distutils.core import setup
 setup(
   name = 'twisted-apns',
   packages = ['apns'],
-  version = '0.3',
+  version = '0.4',
   description = 'Twisted client for Apple Push Notification Service (APNs)',
   author = 'Michał Łowicki',
   author_email = 'mlowicki@opera.com',
   url = 'https://github.com/operasoftware/twisted-apns',
-  download_url = 'https://github.com/operasoftware/twisted-apns/tarball/0.1',
+  download_url = 'https://github.com/operasoftware/twisted-apns/tarball/0.4',
   keywords = ['twisted', 'apns'],
   classifiers = [],
   install_requires=['Twisted', 'pyOpenSSL']
